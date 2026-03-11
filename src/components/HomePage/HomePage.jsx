9a1408eee9a2ad0b54f3c308453cb244e86886a8
Maryna <marinakukusua@gmail.com>
2026-03-11 21:00:08 +0200

    Add carousel

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,5 +1,19 @@
-import {posters} from '../../constants/'
+import Carousel from 'react-material-ui-carousel';
+import { posters } from '../../constants/';
+import { Box } from '@mui/material';
 
 export default function HomePage () {
-  return <div>HomePage</div>;
+  return (
+    <>
+      <Carousel>
+        {posters.map((poster) => {
+          return (
+            <Box key={poster.id}>
+              <img src={poster.url} alt={poster.alt} />
+            </Box>
+          );
+        })}
+      </Carousel>
+    </>
+  );
 }
